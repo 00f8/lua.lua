6dd397a16395eabe92956668ceacc5ba4c402307
ScriptKiddoHomeMade <83893105+ScriptKiddoHomeMade@users.noreply.github.com>
2022-06-11 22:41:52 -0500

    Create userload.lua

---
+++ userload.lua
@@ -0,0 +1,3 @@
+getgenv().name = ".gg/H7zv3CBTyc" -- custom user goes there you have to rejoin to change it again
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptKiddoHomeMade/lua.lua/main/user%20obfuscate.lua"))()
